414643b565953cec63c098dff66748d15fb97f4d
umaisiqbal <malikumais777@gmail.com>
2025-05-19 10:34:29 +0500

    fixesg

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -14,10 +14,9 @@ function Navbar() {
             Umais Iqbal
           </Link>
         </div>
-
         <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100" id="navbar-default">
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#about">
+            <Link className="block px-4 py-3 no-underline outline-none hover:no-underline" href="/#about">
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
             </Link>
           </li>
